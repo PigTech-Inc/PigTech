4564057d937cb7deb6651e81ae9dd71f07f79a0c
icarodante <icaro.menezes@sptech.school>
2022-05-23 13:24:59 -0300

    🐱‍🚀parametrizações para Azure

--- src/database/config.js
+++ src/database/config.js
@@ -3,10 +3,10 @@ var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
 var sqlServerConfig = {
-    user: "user",
-    password: "XXXXX",
-    database: "XXXXX",
-    server: "XXXXX",
+    user: "admin-acquatec-10",
+    password: "#Gfgrupo10",
+    database: "acquatec",
+    server: "svr-acquatec-grupo10.database.windows.net",
     pool: {
         max: 10,
         min: 0,
