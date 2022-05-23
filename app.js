5f11ddbe5f0fc27cee8bd8048ce43b31185b9e2c
vitorfviana <vitor.viana@sptech.school>
2022-05-23 13:37:44 -0300

    nova app.js

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
 // process.env.AMBIENTE_PROCESSO = "desenvolvimento";
-=======
-//process.env.AMBIENTE_PROCESSO = "desenvolvimento";
->>>>>>> 4564057d937cb7deb6651e81ae9dd71f07f79a0c
 process.env.AMBIENTE_PROCESSO = "producao";
 
 var express = require("express");
 var cors = require("cors");
 var path = require("path");
-var PORTA = 3333;
+var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
 
 var app = express();
 
