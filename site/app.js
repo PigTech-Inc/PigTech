04bdc3cbf5677a2e935fd54ea45c6e3c85da52cd
vitorfviana <vitor.viana@sptech.school>
2022-05-25 07:02:43 -0300

    teste6

--- site/app.js
+++ site/app.js
@@ -8,10 +8,10 @@ var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
 
 var app = express();
 
-var indexRouter = require("../src/routes/index");
-var usuarioRouter = require("../src/routes/usuarios");
-var avisosRouter = require("../src/routes/avisos");
-var medidasRouter = require("../src/routes/medidas");
+var indexRouter = require("./src/routes/index");
+var usuarioRouter = require("./src/routes/usuarios");
+var avisosRouter = require("./src/routes/avisos");
+var medidasRouter = require("./src/routes/medidas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
